e1008e1819e8ac18c6c11757bde6b82f00d1aaef
Travis Munson <munson.t@gmail.com>
2017-10-18 11:22:13 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "example" {
+  ami = "ami-cd0f5cb6"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-example"
+  }
+}
