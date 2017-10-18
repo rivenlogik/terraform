7118f8623e9058b1465a36dc8ecadfc71a59bd72
Travis Munson <munson.t@gmail.com>
2017-10-18 12:41:40 -0500

    Add input and output variables

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+variable "server_port" {
+  type = "string"
+  description = "The port the server will use for HTTP requests"
+  default = "8080"
+}
+
 resource "aws_instance" "example" {
   ami = "ami-cd0f5cb6"
   instance_type = "t2.micro"
@@ -9,7 +16,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, world" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
   tags = {
@@ -20,9 +27,13 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
